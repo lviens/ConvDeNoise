3bbb9619d120f1adc3f6c257ba83be5cf3552f65
Loïc Viens <viens.loic.58r@st.kyoto-u.ac.jp>
2019-07-01 14:42:42 +0900

    Update functions_for_autoencoders.py

--- Codes/functions_for_autoencoders.py
+++ Codes/functions_for_autoencoders.py
@@ -6,7 +6,18 @@ from scipy.signal import butter, filtfilt
 
 #%%
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
+    """
+    Two-pass Butterworth filter
+    Inputs: 
+        - data: Data to filter (in time domain)
+        - lowcut: Lowest frequency of the filter (in Hz)
+        - highcut: Highest frequency of the filter (in Hz)
+        - fs: Sampling rate (in Hz)
+        - Order of the filter (4)
+    Output:
+        y: Filtered data
     
+    """
     nyq = 0.5 * fs
     low = lowcut / nyq
     high = highcut / nyq
@@ -17,14 +28,15 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
 
 def ncf_denoise(img_to_denoise, mdate, ntau, nsv, nsv_to_rm, use_wiener):
     """
+    SVDWF method from Moreau et al (2017)
     Inputs:
-        img_to_denoise: the list of NCF. It should be an MxN matrix where M represents the total number of NCF and N the
+        - img_to_denoise: the list of NCF. It should be an MxN matrix where M represents the total number of NCF and N the
             length of each NCF
-        mdate: the size of the Wiener filter in the first dimension (typically between 5 and 10)
-        ntau: the size of the Wiener filter in the second dimension (typically between 5 and 10)
-        nsv: the number of singular values to keep in the SVD filter (typically less than 50)
+        - mdate: the size of the Wiener filter in the first dimension (K = 3)
+        - ntau: the size of the Wiener filter in the second dimension (L = 5)
+        - nsv: the number of singular values to keep in the SVD filter (25 
     Outputs:
-        denoised_img: the denoised list of NCF
+        - denoised_img: the denoised list of NCF
     """
     if nsv > min(np.shape(img_to_denoise)):
         nsv = min(np.shape(img_to_denoise))
@@ -45,39 +57,25 @@ def ncf_denoise(img_to_denoise, mdate, ntau, nsv, nsv_to_rm, use_wiener):
     return denoised_img
 
 
-def ncf_SVD(img_to_denoise, nsv):
+
+def stretching_current(ref, cur, dvmin, dvmax, nbtrial, window,t_vec):
     """
-    Inputs:
-        img_to_denoise: the list of NCF. It should be an MxN matrix where M represents the total number of NCF and N the
-            length of each NCF
-        mdate: the size of the Wiener filter in the first dimension (typically between 5 and 10)
-        ntau: the size of the Wiener filter in the second dimension (typically between 5 and 10)
-        nsv: the number of singular values to keep in the SVD filter (typically less than 50)
-    Outputs:
-        denoised_img: the denoised list of NCF
-    """
-    if nsv > min(np.shape(img_to_denoise)):
-        nsv = min(np.shape(img_to_denoise))
+    Function to perform the stretching of the waveforms: 
+    This function compares the Reference waveform to stretched/compressed current waveforms to get the relative seismic velocity variation (and associated error).
+    It also computes the correlation coefficient between the Reference waveform and the current waveform.
+    INPUTS:
+        - ref = Reference waveform (np.ndarray, size N)
+        - cur = Current waveform (np.ndarray, size N)
+        - dvmin = minimum bound for the velocity variation; example: dvmin=-0.03 for -3% of relative velocity change ('float')
+        - dvmax = maximum bound for the velocity variation; example: dvmax=0.03 for 3% of relative velocity change ('float')
+        - nbtrial = number of stretching coefficient between dvmin and dvmax, no need to be higher than 50  ('float')
+        - window = vector of the indices of the cur and ref windows on wich you want to do the measurements (np.ndarray, size tmin*delta:tmax*delta)
+        - t_vec = time vector, common to both ref and cur (np.ndarray, size N)
 
-    U, s, V = np.linalg.svd(img_to_denoise)
-    Xwiener = np.zeros((np.shape(img_to_denoise)))
-    plt.figure()
-    plt.plot(s)
-    plt.title('singular values SVD')
-    plt.show()
-    for kk in np.arange(0, nsv):
-        SV = np.zeros((img_to_denoise.shape))
-        SV[kk, kk] = s[kk]
-        X = U @ SV @ V  # equivalently, U.dot(SV.dot(V))
-        Xwiener = X + Xwiener
-
-    denoised_img = Xwiener
-    return denoised_img
-
-
-def stretching_current(ref, cur, t, dvmin, dvmax, nbtrial, window,t_vec):
-    """
-        Stretching
+    OUTPUTS:
+        - dv = Relative velocity change dv/v (in %)
+        - cc = correlation coefficient between the reference waveform and the best stretched/compressed current waveform
+        - Eps = Vector of Epsilon values (Epsilon =-dt/t = dv/v)
     """
     Eps = np.asmatrix(np.linspace(dvmin, dvmax, nbtrial))
     L = 1 + Eps
@@ -101,7 +99,8 @@ def stretching_current(ref, cur, t, dvmin, dvmax, nbtrial, window,t_vec):
     func = scipy.interpolate.interp1d(np.ravel(Eps[0,np.arange(imax-2, imax+2)]), np.ravel(C[0,np.arange(imax-2, imax+2)]), kind='cubic')
     CCfiner = func(dtfiner)
     cc = np.max(CCfiner) # Find maximum correlation coefficient of the refined  analysis
-    dv = 100 * dtfiner[np.argmax(CCfiner)] 
+    dv = 100 * dtfiner[np.argmax(CCfiner)]  # Final dv/v measurement (x100 to get the dv/v in %)
     
 
     return dv, cc, Eps
+
